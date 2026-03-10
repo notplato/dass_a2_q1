9e39e86b7a843be57ec2d77cd287c4762e5d0c48
Srinivas Kolla <kolla.srinag@gmail.com>
2026-03-10 08:31:56 +0000

    Iteration 4: refactored player module to combine all jail properties

--- moneypoly/moneypoly/player.py
+++ moneypoly/moneypoly/player.py
@@ -13,9 +13,11 @@ class Player:
         self.balance = balance
         self.position = 0
         self.properties = []
-        self.in_jail = False
-        self.jail_turns = 0
-        self.get_out_of_jail_cards = 0
+        self.jail_props = {
+            "in_jail" : False,
+            "jail_turns" : 0,
+            "get_out_of_jail_cards" : 0,
+        }
         self.is_eliminated = False
 
 
@@ -56,8 +58,8 @@ class Player:
     def go_to_jail(self):
         """Send this player directly to the Jail square."""
         self.position = JAIL_POSITION
-        self.in_jail = True
-        self.jail_turns = 0
+        self.jail_props["in_jail"] = True
+        self.jail_props["jail_turns"] = 0
 
 
     def add_property(self, prop):
@@ -77,7 +79,7 @@ class Player:
 
     def status_line(self):
         """Return a concise one-line status string for this player."""
-        jail_tag = " [JAILED]" if self.in_jail else ""
+        jail_tag = " [JAILED]" if self.jail_props["in_jail"] else ""
         return (
             f"{self.name}: ${self.balance}  "
             f"pos={self.position}  "
